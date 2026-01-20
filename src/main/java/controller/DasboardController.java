3d60aab62220421bf79e3a30ba870ac876a6c9c1
AksaMJML <akaks0186@gmail.com>
2026-01-20 23:46:39 +0530

    created ui forms and object controllers classes and linked the path to every page

--- src/main/java/controller/DasboardController.java
+++ src/main/java/controller/DasboardController.java
@@ -2,22 +2,54 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DasboardController {
 
+    Stage stage = new Stage();
     @FXML
     void btnCustomerOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnItemOnAction(ActionEvent event) {
-
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/customer_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
     }
 
     @FXML
     void btnOrderOnAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/order_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
 
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/item_form.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
+
+
+    @FXML
+    void btnOrderDetailsOnAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/order_detail.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
     }
 
 }
