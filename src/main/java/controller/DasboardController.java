51da950b31c063115c8cac874cbc3ceec5e033fe
AksaMJML <akaks0186@gmail.com>
2026-01-20 23:28:21 +0530

    dsshboard sample components done

--- src/main/java/controller/DasboardController.java
+++ src/main/java/controller/DasboardController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class DasboardController {
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+    }
+
 }
