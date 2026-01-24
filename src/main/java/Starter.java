52f6392ca54a905f60006d37973fb4f8478707fe
AksaMJML <akaks0186@gmail.com>
2026-01-24 19:20:23 +0530

    Refactor UI components in customer and item forms; enhance button styles and layout

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,7 +11,22 @@ public class Starter extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/login_form.fxml"))));
+//        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/login_form.fxml"))));
+//        Scene scene = new Scene(FXMLLoader.load(), 320, 480);
+//        scene.getStylesheets().add(getClass().getResource("view/style.css").toExternalForm());
+//        stage.show();
+
+        Parent root = FXMLLoader.load(
+                getClass().getResource("view/login_form.fxml")
+        );
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(
+                getClass().getResource("view/style.css").toExternalForm()
+        );
+
+        stage.setScene(scene);
         stage.show();
+
     }
 }
