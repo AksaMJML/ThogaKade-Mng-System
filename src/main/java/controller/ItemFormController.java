cd1b18b0500fa257c6044be589bfd5554ba6b0fb
AksaMJML <akaks0186@gmail.com>
2026-01-24 20:48:58 +0530

    Enhance dashboard UI with improved styles and add item form controller methods

--- src/main/java/controller/ItemFormController.java
+++ src/main/java/controller/ItemFormController.java
@@ -1,4 +1,45 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ItemFormController {
+
+    @FXML
+    private TextArea txtAreaDescription;
+
+    @FXML
+    private TextField txtCode;
+
+    @FXML
+    private TextField txtPackSize;
+
+    @FXML
+    private TextField txtQtyOnHand;
+
+    @FXML
+    private TextField txtUnitPrice;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
