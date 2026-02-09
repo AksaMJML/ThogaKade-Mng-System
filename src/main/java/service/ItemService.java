1e582a0b2e3766c7db3da8bcd809539b4f479715
AksaMJML <akaks0186@gmail.com>
2026-02-09 14:16:27 +0530

    Refactor ItemService methods to accept ItemDTO for add and update operations

--- src/main/java/service/ItemService.java
+++ src/main/java/service/ItemService.java
@@ -4,8 +4,8 @@ import javafx.collections.ObservableList;
 import model.dto.ItemDTO;
 
 public interface ItemService {
-    void addItem(String code, String description, String packSize, double unitPrice, int qtyOnHand);
-    void updateItem(String code, String description, String packSize, double unitPrice, int qtyOnHand);
+    void addItem(ItemDTO itemDTO);
+    void updateItem(ItemDTO itemDTO);
     void deleteItem(String code);
     ObservableList<ItemDTO> getAllItems();
 }
